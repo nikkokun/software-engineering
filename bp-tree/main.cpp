d74291b78ce8fb441ba9510423c5e6b3012298b9
nikkokun <mizutaninikkou@gmail.com>
2020-02-06 03:40:33 +0900

    Add tests

--- bp-tree/main.cpp
+++ bp-tree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <bplustree.h>
 
 using namespace std;
@@ -7,29 +8,38 @@ using namespace bptree;
 
 int main() {
 
+  set<int> numbers;
+  while (numbers.size() < 20)
+  {
+    numbers.insert(rand() % 100000);
+  }
   auto bptree = new BPlusTree();
-  for (int i = 1; i < 20; ++i) {
-    bptree->insert(i);
+  for(auto number : numbers) {
+    bptree->insert(number);
     bptree->print_tree();
   }
 
-
-  delete bptree;
-
   auto bptree2 = new BPlusTree();
-  for (int i = 30; i > 0; --i) {
+  for (int i = 1; i < 20; ++i) {
     bptree2->insert(i);
     bptree2->print_tree();
   }
 
-  delete bptree2;
 
   auto bptree3 = new BPlusTree();
-  for (int i = 1; i < 1000000; ++i) {
+  for (int i = 30; i > 0; --i) {
     bptree3->insert(i);
+    bptree3->print_tree();
   }
 
+  auto bptree4 = new BPlusTree();
+  for (int i = 1; i < 1000000; ++i) {
+    bptree4->insert(i);
+  }
+  delete bptree;
+  delete bptree2;
   delete bptree3;
+  delete bptree4;
 
   return 0;
 }
